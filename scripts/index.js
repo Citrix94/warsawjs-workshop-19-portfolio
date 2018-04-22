34abdd8cd2e50507795bd125e2a2c5e4465c285f
Citrix94 <kontakt@adamrogulski.com>
2018-04-22 21:21:01 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
+// "name" jako wbudowana właściwość obiektu window tworzy wyjątek i niezależnie jakiego typu wartość do niej przypiszmy zawsze będzie stringiem. 
+// Zeruje się dopiero w momencie przeładowania przeglądarki.
+
+window.name = 102;
+console.log(typeof window.name);
+console.log(typeof 102);
+
+const czolg = 102;
+console.log(typeof czolg);
+
 const $form = document.querySelector('form'); // dolar przy zmiennych, które przechowują referencje do DOM
 
 function displayMessage(message) { // tworzymy funkcję, do której parametru przekazujemy argument z funkcji niżej
 
-    alert(`Na ten email ${message} został wysłany kup rabatowy`); // template string (stringi szablonowe)
+    alert(`Na ten email ${name} ${message} został wysłany kup rabatowy`); // template string (stringi szablonowe)
 
 };
 
-$form.addEventListener('submit', function (evt) {
+$form.addEventListener('submit', function (evt) { // dodajemy słuchacza zdarzeniowego - oczekuje ona dwóch parametrów - 1 na jaki event, 2 - co uruchomić
 
     evt.preventDefault(); // wyłączamy domyślną funkcję formularza
 
@@ -20,7 +30,7 @@ $form.addEventListener('submit', function (evt) {
 
     displayMessage(email);
 
-}); // dodajemy słuchacza zdarzeniowego - oczekuje ona dwóch parametrów - 1 na jaki event, 2 - co uruchomić
+}); 
 
 
 
