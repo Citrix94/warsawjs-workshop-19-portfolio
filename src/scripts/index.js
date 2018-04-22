5bfe9388a9023de76231cc686a3d7fa33fa62d00
Citrix94 <kontakt@adamrogulski.com>
2018-04-22 17:08:59 +0200

    change JS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,15 @@ const $form = document.querySelector('form'); // dolar przy zmiennych, które pr
 
 $form.addEventListener('submit', function (evt) {
 
-   evt.preventDefault(); // wyłączamy domyślną funkcję formularza
+    evt.preventDefault(); // wyłączamy domyślną funkcję formularza
+
+    const data = new FormData($form); // uruchamiamy konstruktor, który umożliwi nam pobranie danych z formularza
+
+    const map = new Map(data);// tworzymy zmienną, w któej będziemy mieli mapę parametrów, czyli zbiór gdzie mamy klucz-wartość, klucz-wartość
+
+    const email = map.get('email');// z mapy możemy wyciagnąć nasz email
+
+    console.log(email);
 
 }); // dodajemy słuchacza zdarzeniowego - oczekuje ona dwóch parametrów - 1 na jaki event, 2 - co uruchomić
 
